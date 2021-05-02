699e89dcf479b1983bc21e15d038f0febce9495e
Tor Magne <61709493+TorMagne@users.noreply.github.com>
2021-05-02 15:48:14 +0200

    added loader animation and done with upcoming site

--- js/upcomingLaunches.js
+++ js/upcomingLaunches.js
@@ -7,13 +7,12 @@ const items = document.querySelector('.items');
 launchesContainer.innerHTML = `<div class="loader"></div>`;
 
 // links
-const pastLaunchesUrl = 'https://api.spacexdata.com/v4/launches/past';
+const upcomingUrl = 'https://api.spacexdata.com/v4/launches/upcoming';
 
 const pastLaunches = async () => {
   try {
-    const response = await fetch(pastLaunchesUrl);
+    const response = await fetch(upcomingUrl);
     const json = await response.json();
-    json.reverse();
     console.log(json);
 
     launchesContainer.innerHTML = '';
@@ -23,22 +22,10 @@ const pastLaunches = async () => {
       const name = launchLoop.name;
       const date = launchLoop.date_utc;
       const newDate = date.split('T')[0];
-      const succsess = launchLoop.success;
-      const link = launchLoop.links.webcast;
-
-      const lol = () => {
-        if (succsess) {
-          return `<span class="green">Succsess</span>`;
-        } else {
-          return `<span class="red">Failure</span>`;
-        }
-      };
 
       launchesContainer.innerHTML += `<div class="launches__items">
                                         <p class="launches__title">Mission: ${name}</p>
-                                        <p class="launches__time">Launched at: ${newDate}</p>
-                                        <a href="${link}" class="launches__link">Launch video</a>
-                                        <p>Launch: ${lol()}</P>
+                                        <p class="launches__time">Launch date: ${newDate}</p>
                                       </div>`;
     });
   } catch (error) {
