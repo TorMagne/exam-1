cca05f6a2b0dd39c37cc7020ba2fde12603c990d
Tor Magne <61709493+TorMagne@users.noreply.github.com>
2021-05-03 17:32:17 +0200

    fixed form bug

---
+++ js/form.js
@@ -0,0 +1,72 @@
+const form = document.querySelector('#contact-form');
+const name = document.querySelector('#name');
+const nameError = document.querySelector('#nameError');
+const subject = document.querySelector('#subject');
+const subjectError = document.querySelector('#subjectError');
+const address = document.querySelector('#address');
+const addressError = document.querySelector('#addressError');
+const email = document.querySelector('#email');
+const emailError = document.querySelector('#emailError');
+const succsess = document.querySelector('.succsess');
+
+function formValidation(event) {
+  event.preventDefault();
+
+  let validationFailure = true;
+
+  if (checkLength(name.value, 0) === true) {
+    nameError.style.display = 'none';
+    validationFailure = true;
+  } else {
+    nameError.style.display = 'block';
+    validationFailure = false;
+  }
+
+  if (checkLength(subject.value, 9) === true) {
+    subjectError.style.display = 'none';
+    validationFailure = true;
+  } else {
+    subjectError.style.display = 'block';
+    validationFailure = false;
+  }
+
+  if (checkLength(address.value, 24) === true) {
+    addressError.style.display = 'none';
+    validationFailure = true;
+  } else {
+    addressError.style.display = 'block';
+    validationFailure = false;
+  }
+
+  if (validateEmail(email.value) === true) {
+    emailError.style.display = 'none';
+    validationFailure = true;
+  } else {
+    emailError.style.display = 'block';
+    validationFailure = false;
+  }
+
+  if (validationFailure === true) {
+    succsess.style.display = 'block';
+  } else {
+    succsess.style.display = 'none';
+  }
+
+  // return true;
+}
+
+form.addEventListener('submit', formValidation);
+
+function checkLength(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const matchPattern = regEx.test(email);
+  return matchPattern;
+}
